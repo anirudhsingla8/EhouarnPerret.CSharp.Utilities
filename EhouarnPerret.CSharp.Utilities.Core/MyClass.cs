5aa120d80a08bf3c895a30b5372a9cd98b80096e
Ehouarn Perret <ehouarn.perret@outlook.com>
2015-10-17 14:07:50 +0800

    * EhouarnPerret.CSharp.Utilities.Core.csproj: Solution and Projects structure
    * Test.cs:
    * MyClass.cs:
    * packages.config:
    * AssemblyInfo.cs:
    * EhouarnPerret.CSharp.Utilities.Test.csproj:

---
+++ EhouarnPerret.CSharp.Utilities.Core/MyClass.cs
@@ -0,0 +1,17 @@
+﻿// Author: Ehouarn Perret
+// Email: ehouarn.perret@art-logics.com
+// Company: ART logics
+// Website: http://www.art-logics.com
+// Copyright: ART logics © All Right Reserved.
+using System;
+
+namespace EhouarnPerret.CSharp.Utilities.Core
+{
+    public class MyClass
+    {
+        public MyClass()
+        {
+        }
+    }
+}
+
---
+++ EhouarnPerret.CSharp.Utilities.Test/Test.cs
@@ -0,0 +1,20 @@
+﻿// Author: Ehouarn Perret
+// Email: ehouarn.perret@art-logics.com
+// Company: ART logics
+// Website: http://www.art-logics.com
+// Copyright: ART logics © All Right Reserved.
+using NUnit.Framework;
+using System;
+
+namespace EhouarnPerret.CSharp.Utilities.Test
+{
+    [TestFixture()]
+    public class Test
+    {
+        [Test()]
+        public void TestCase()
+        {
+        }
+    }
+}
+
