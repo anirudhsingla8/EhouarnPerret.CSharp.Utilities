5c1d7327798d571c64690845d1e3a7adb1d6e05b
Ehouarn Perret <ehouarn.perret@outlook.com>
2015-10-19 00:04:28 +0800

    Centering for Winforms Controls  as Extension Methods

--- EhouarnPerret.CSharp.Utilities.Core/Windows/Forms/ControlExtensions.cs
+++ EhouarnPerret.CSharp.Utilities.Core/Windows/Forms/ControlExtensions.cs
@@ -19,19 +19,35 @@ namespace EhouarnPerret.CSharp.Utilities.Core
 {
     public static class ControlExtensions
     {
+        /// <summary>
+        /// Centers the control both horizontially and vertically 
+        /// according to the parent control that contains it.
+        /// </summary>
+        /// <param name="control"></param>
         public static void Center(this Control control)
         {
-            
+            control.CenterHorizontally();
+            control.CenterVertically();
         }
 
+        /// <summary>
+        /// Centers the control horizontially according 
+        /// to the parent control that contains it.
+        /// </summary>
         public static void CenterHorizontally(this Control control)
         {
-            
+            var parentClientRectangle = control.Parent.ClientRectangle;
+            control.Left = (parentClientRectangle.Width - control.Width) / 2;
         }
 
+        /// <summary>
+        /// Centers the control vertically according 
+        /// to the parent control that contains it.
+        /// </summary>
         public static void CenterVertically(this Control control)
         {
-            
+            var parentClientRectangle = control.Parent.ClientRectangle;
+            control.Top = (parentClientRectangle.Height - control.Height) / 2;
         }
     }
 }
