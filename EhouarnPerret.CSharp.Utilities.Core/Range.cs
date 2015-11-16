79e477239b11a04a441c0ec1ec1cc3138838a306
Ehouarn Perret <ehouarn.perret@outlook.com>
2015-11-17 03:17:06 +0800

    Range WIP, good night

--- EhouarnPerret.CSharp.Utilities.Core/Range.cs
+++ EhouarnPerret.CSharp.Utilities.Core/Range.cs
@@ -17,10 +17,72 @@ using System.Collections;
 
 namespace EhouarnPerret.CSharp.Utilities.Core
 {
-    public class Range<T>
-        // where T : IComparer<T>, ICompara
+//    interface IRange<out T>
+//        where T : IComparable<T>
+//    {
+//        T UpperBound { get; }
+//        T LowerBound { get; } 
+//
+//        Boolean Contains(T value);
+//        Boolean Contains(IRange<T> range);
+//        Boolean Overlaps(IRange<T> range);
+//    }
+
+    public sealed class Range<T>
+        where T : IComparable<T>
     {
-        
+        public T UpperBound { get; }
+        public T LowerBound { get; }
+
+        public Range(T lowerBound, T upperBound)
+        {
+            this.LowerBound = ExceptionHelpers.ThrowIfNull(lowerBound, nameof(lowerBound));
+            this.UpperBound = ExceptionHelpers.ThrowIfNull(upperBound, nameof(upperBound));
+        }
+
+        public Boolean Contains(T value)
+        {
+            var isValueLowered = this.LowerBound.CompareTo(value) <= 0;
+            var isValueUppered = this.UpperBound.CompareTo(value) >= 0;
+            
+            return isValueLowered && isValueUppered;
+        }
+
+        public Boolean Contains(Range<T> range)
+        {
+            var isRangeLowered = this.LowerBound.CompareTo(range.LowerBound) <= 0;
+            var isRangeUppered = this.UpperBound.CompareTo(range.UpperBound) >= 0;
+
+            return isRangeLowered && isRangeUppered;
+        }
+
+        public Boolean IsContainedBy(Range<T> range)
+        {
+            return range.Contains(this);
+        }
+
+        public Boolean Overlaps(Range<T> range)
+        {
+            return this.Contains(range.LowerBound) || 
+                this.Contains(range.UpperBound) || 
+                range.Contains(this.LowerBound) || 
+                range.Contains(this.UpperBound);
+        }
+
+        public Range<T> Intersect(Range<T> value)
+        {
+            if (this.Overlaps(value))
+            {
+                var lowerBound = (this.LowerBound.CompareTo(value.LowerBound) > 0) ? this.LowerBound : value.LowerBound;
+                var upperBound = (this.UpperBound.CompareTo(value.UpperBound) < 0) ? this.UpperBound : value.UpperBound;
+            
+                return new Range<T>(lowerBound, upperBound);
+            }
+            else
+            {
+                throw new ArgumentOutOfRangeException(nameof(value))'
+            }
+        }
     }
 }
 
