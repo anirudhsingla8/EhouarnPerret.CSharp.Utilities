af070dee8b8b16c017653b06a72884611bd7fcd9
Ehouarn Perret <ehouarn.perret@outlook.com>
2015-11-21 23:59:16 +0800

    Quick Sort

--- EhouarnPerret.CSharp.Utilities.Harness/Program.cs
+++ EhouarnPerret.CSharp.Utilities.Harness/Program.cs
@@ -13,15 +13,8 @@
 //    See the License for the specific language governing permissions and
 //    limitations under the License.
 using System;
-using System.Collections.Generic;
-using EhouarnPerret.CSharp.Utilities.Core;
 using System.Linq;
-using System.Linq.Expressions;
-using System.Windows.Forms;
-using System.Dynamic;
-using System.Configuration;
-using System.Net;
-using System.ComponentModel;
+using EhouarnPerret.CSharp.Utilities.Core;
 
 namespace EhouarnPerret.CSharp.Utilities.Harness
 {
@@ -29,98 +22,17 @@ namespace EhouarnPerret.CSharp.Utilities.Harness
     {
         public static void Main(params String[] arguments)
         {
+            var ints = new [] { 1, 4, 5 ,2 , 0, 9 ,3 , 42, 69, -5 };
 
-            // Mono Databindings Support... WTF?
-            var form = new Form();
+            var intsString = String.Join(@",", ints.Select(item => item.ToString()));
 
-            var content = new Content();
+            Console.WriteLine( String.Join(@",", ints.Select(item => item.ToString())));
 
-            var button = new Button() 
-            { 
-                Dock =  DockStyle.Top, 
-            };
+            var re = SortingHelpers.Quicksort(ints);
 
-            button.Click += (sender, e) =>
-            {
-                content.String += "meuh";
-                Console.WriteLine(content.String);
-            };
+            Console.WriteLine( String.Join(@",", re.Select(item => item.ToString())));
 
-            content.PropertyChanged += (sender, e) => Console.WriteLine("42");
-            
-            var binding = button.DataBindings.Add(@"Text", content, @"String", true, DataSourceUpdateMode.OnPropertyChanged, String.Empty);
-                        
-            binding.BindingComplete += (sender, e) => Console.WriteLine(@"BindingComplete");
-            binding.Format += (sender, e) => Console.WriteLine(@"Format");
-            binding.Parse += (sender, e) => Console.WriteLine(@"Parse");
-
-            content.AnotherThing.String = @"d";
-
-            form.Controls.Add(button);
-
-            Application.Run(form);
+            Console.ReadKey();
         }
-    }
-
-    public static class BindingHelpers
-    {
-
-    }
-
-
-    public class Content : INotifyPropertyChanged
-    {
-        AnotherThing _anotherThing = new AnotherThing();
-        public AnotherThing AnotherThing
-        {
-            get
-            {
-                return _anotherThing;
-            }
-            set
-            {
-                _anotherThing = value;
-                this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(@"AnotherThing"));
-            }
-        }
-        
-        #region INotifyPropertyChanged implementation
-        public event PropertyChangedEventHandler PropertyChanged;
-        #endregion
-
-        String _string = @"Hello";
-        public String String
-        {
-            get
-            {
-                return _string;
-            }
-            set
-            {
-                _string = value;
-                this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(@"String"));
-            }
-        } 
-    }
-
-    public class AnotherThing : INotifyPropertyChanged
-    {
-        #region INotifyPropertyChanged implementation
-        public event PropertyChangedEventHandler PropertyChanged;
-        #endregion
-
-        String _string = @"Hello";
-        public String String
-        {
-            get
-            {
-                return _string;
-            }
-            set
-            {
-                _string = value;
-                this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(@"String"));
-            }
-        } 
     }
 }
