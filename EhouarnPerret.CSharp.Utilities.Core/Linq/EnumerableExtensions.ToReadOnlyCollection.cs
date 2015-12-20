e2bb02ff647393b4fbc0135f75ce188ecff0882b
Ehouarn Perret <ehouarn.perret@outlook.com>
2015-12-20 23:20:18 +0800

    * EnumerableExtensions.ToReadOnlyCollection.cs:
    * EhouarnPerret.CSharp.Utilities.Core.csproj: ToReadOnlyCollection
      extension method

---
+++ EhouarnPerret.CSharp.Utilities.Core/Linq/EnumerableExtensions.ToReadOnlyCollection.cs
@@ -0,0 +1,49 @@
+﻿//
+// EnumerableExtensions.ToReadOnlyCollection.cs
+//
+// Author:
+//       Ehouarn Perret <ehouarn.perret@outlook.com>
+//
+// Copyright (c) 2015 Ehouarn Perret
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace EhouarnPerret.CSharp.Utilities.Core.Linq
+{
+    public static partial class EnumerableExtensions
+    {
+        public static ReadOnlyCollection<TSource> ToReadOnlyCollection<TSource>(IEnumerable<TSource> source)
+        {
+            var readOnlyCollection = new ReadOnlyCollection<TSource>(source.ToList());
+
+            return readOnlyCollection;
+        }
+        public static ReadOnlyCollection<TResult> ToReadOnlyCollection<TSource, TResult>(IEnumerable<TSource> source, Func<TSource, TResult> resultSelector)
+        {
+            var readOnlyCollection = new ReadOnlyCollection<TSource>(source.ToList(resultSelector));
+
+            return readOnlyCollection;
+        }
+    }
+}
+
